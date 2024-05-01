0c6f140739754863fa3abba639162793d8ca3fee
Taha <muhammadtaha592002@gmail.com>
2024-05-01 16:19:37 +0500

    completed

---
+++ app.js
@@ -0,0 +1,9 @@
+function on(){
+    document.getElementById("imgOff").style.display="none"
+    document.getElementById("imgOn").style.display="block"
+}
+
+function off(){
+    document.getElementById("imgOn").style.display="none"
+    document.getElementById("imgOff").style.display="block"
+}
